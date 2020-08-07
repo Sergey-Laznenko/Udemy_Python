07d7c53c9b58ee073266adb008d59a3d9c380bda
Sergey Laznenko <61808964+Sergey-Laznenko@users.noreply.github.com>
2020-08-07 20:18:57 +0300

    Add files via upload

---
+++ Section_2/task_1.py
@@ -0,0 +1,12 @@
+
+# Задача 1
+# 1st option
+prompt = input('Enter the number of cups: ')
+cups = int(prompt)
+free_cups = int(cups / 6)
+print(free_cups)
+
+# 2nd option
+cups = int(input('Enter the number of cups: '))
+print(int(cups / 6))
+
